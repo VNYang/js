3b4069df5b5fe7d269f5c101cdadf7d81faae2c0
yangwanxian <595618396@qq.com>
2018-06-28 20:20:14 +0800

    ..

--- js/work.js
+++ js/work.js
@@ -14,7 +14,7 @@ function isLeapyear(year){
     if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
         return true;
     }
-    return false;
+    return false;//...
 }
 
     function countDays(year,month,day){
